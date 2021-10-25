5c86c1dbc37c8e5fed2eedc833b5f8cdc2f9aa22
tatianagutierrez <tatiana7gutierrez@gmail.com>
2021-10-25 20:30:51 -0300

    primer commit

---
+++ Proyecto1.py
@@ -0,0 +1,62 @@
+def expreRegulares():
+    print("Punto 1\n")
+    #resolucion
+
+    print("Punto 2\n")
+    #resolucion
+
+def recursion():
+    print("Punto 1\n")
+    #resolucion
+
+    print("Punto 2\n")
+    #resolucion
+
+    print("Punto 3\n")
+    #resolucion
+
+    print("Punto 4\n")
+    #resolucion
+
+def colecciones():
+    print("Punto 1\n")
+    #resolucion
+
+    print("Punto 2\n")
+    #resolucion
+
+def intercambioDatos():
+    print("Punto 1\n")
+    #resolucion
+
+    print("Punto 2\n")
+    #resolucion
+
+print("MENU DEL PROGRAMA")
+print("---------------------------")
+menu = {1: "Expresiones regulares", 2: "Recursion", 3: "Colecciones", 4: "Formato de intercambios de datos", 0: "Salir"}
+
+# Este while pretende simular un switch case
+while True: 
+    # Se imprime el menu
+    opcionKey = menu.keys()
+    for i in opcionKey:
+        print("[", i, "]", menu[i])
+
+    opcion = int(input("Que ejercicio desea ver?: "))
+    if opcion == 1:
+        expreRegulares()
+    elif opcion == 2:
+        recursion()
+    elif opcion == 3:
+        colecciones()
+    elif opcion == 4:
+        intercambioDatos()
+    elif opcion == 0:
+        break
+    else:
+        print("Error. La opcion es incorrecta. Intentelo nuevamente\n")
+
+print("\nPrograma finalizado")
+
+    
